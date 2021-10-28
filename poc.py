08c3424eed6d10577407fc1ffb1479d0abd9d632
aschneeberger <antoine.schneeberger@protonmail.com>
2021-10-28 17:02:28 +0200

    [untested] eq sys residues+kappa not independt var

--- poc.py
+++ poc.py
@@ -9,7 +9,7 @@ import numpy as np
 from numpy.core.numeric import full
 from numpy.typing import _256Bit 
 import scipy.optimize as opt 
-from scipy.integrate import solve_ivp
+import scipy.integrate as  int
 import astropy.constants as cte
 from scipy.optimize._lsq.least_squares import soft_l1
 from scipy.special import erfinv
@@ -123,10 +123,9 @@ def cap_lambda (r,Rc,Rp,R_disk) :
 ##################################################################################################
 
 """
-The system to solve is derived fomr Makalkin et al 1995 and 2014, there are 9 equations to solbve 
-with 9 unknowns: 
+The system to solve is derived fomr Makalkin et al 1995 and 2014, there are 8 equations to solbve 
+with 8 unknowns: 
     Σ: the surface density 
-    Κs: planck mean opacity at the disk potosphere 
     ρm: mid-plane density 
     ρs: photosphere density
     ρa: addiabatique to isotherme frontier density
@@ -199,7 +198,7 @@ def midplane_temp_residue(mid_temp_guess,temp_surf,gamma,Chi,kappa0,beta,mug,alp
 #-----------------------------Residue vector construction----------------------------------# 
 """
 The method from scipy only take 1D vectors, ones have to parse the values on the grid from 
-a 9 by Nr matrix to a 9*Nr vector. 
+a 8 by Nr matrix to a 8*Nr vector. 
 The résidue compute the vector function F[X]=R, where R is the residue vectore to minimize to 0 
 Such that the method solve the equation F[X]=0.  
 """
@@ -209,7 +208,7 @@ def Parse(X,Nr):
     Parser from the Vector form to the matrix form.
 
     input:
-    X:variable vector of size 9*Nr
+    X:variable vector of size 8*Nr
 
     output:
     dict_var: dict with variable names as keywords and values as output of size Nr 
@@ -217,14 +216,13 @@ def Parse(X,Nr):
     dict_var = {}
 
     dict_var['sigma'] = X[:Nr]
-    dict_var['kappa_p'] = X[Nr:2*Nr]
-    dict_var['T_mid'] = X[2*Nr:3*Nr]
-    dict_var['T_s'] = X[3*Nr:4*Nr]
-    dict_var['z_s'] = X[4*Nr:5*Nr]
-    dict_var['z_add'] = X[5*Nr:6*Nr]
-    dict_var['rho_mid'] = X[6*Nr:7*Nr]
-    dict_var['rho_add'] = X[7*Nr:8*Nr] 
-    dict_var['rho_s'] = X[8*Nr:]
+    dict_var['T_mid'] = X[Nr:2*Nr]
+    dict_var['T_s'] = X[2*Nr:3*Nr]
+    dict_var['z_s'] = X[3*Nr:4*Nr]
+    dict_var['z_add'] = X[4*Nr:5*Nr]
+    dict_var['rho_mid'] = X[5*Nr:6*Nr]
+    dict_var['rho_add'] = X[6*Nr:7*Nr] 
+    dict_var['rho_s'] = X[7*Nr:]
 
     return dict_var
 
@@ -234,16 +232,80 @@ def Serialize(dict_var):
     Convert the Matrix to the Vector form
     """
 
-    return np.concatenate((dict_var['sigma'], dict_var['kappa_p'], dict_var['T_mid'], dict_var['T_s'], dict_var['z_s'], dict_var['z_add'], dict_var['rho_mid'], dict_var['rho_add'], dict_var['rho_s']))
+    return np.concatenate((dict_var['sigma'], dict_var['T_mid'], dict_var['T_s'], dict_var['z_s'], dict_var['z_add'], dict_var['rho_mid'], dict_var['rho_add'], dict_var['rho_s']))
 
 
 def Residue(X,dict_cte,N) :
     """
-    Compute the equation system vector residues F[X]  
+    Compute the equation system vector residues F[X] 
+    The equations are equation 10,17,23,24,31-32,36,37 and 39 + Κs opacity table from makalkin et al 1995
     """
 
-    #Values from previous iteration 
+    #Values from the previous iteration 
+    dict_var = Parse(X,Nr)
+
+    # Mean planck opacity at photosphere heigh 
+    kappa_p0 , beta, kappa_p = mean_planck_opacity(dict_var['T_s'], dict_cte['Chi'])
+
+    #Eq 10: Accretion rate 
+
+    res_10 = dict_cte['Mdot'] - (3*np.pi * dict_cte['gamma'] * 8.314 * dict_cte['alpha'] * dict_var['sigma'] * dict_var['T_mid'] * dict_cte['L']) / (dict_cte['omegak'] * dict_cte['mu_gas'] * dict_cte['cap_lambda']) 
+
+    #Eq 17: Surface temperature with improvemnt from heller 2015 
+
+    # Planet luminosity energy flux
+    flux_p = F_planet(dict_cte['L_p'],dict_cte['R_p'],dict_var['z_s'],dict_cte['r'])    
+
+    res_17 = dict_var['T_s'] - surface_temperature(dict_var['sigma'],dict_cte['F_vis'],dict_cte['F_acc'],flux_p,dict_cte['Ks'],dict_cte['temp_neb'],kappa_p)
+
+    #Eq 23:  Addiabatique altitude gas density 
     
+    c_s = np.sqrt(dict_cte['gamma'] * 8.314 * dict_var['T_mid'] / dict_cte['mu_gas']) # mid plane sound speed 
+    h = cs / dict_cte['omegak'] # disk scale heghy
+
+    res_23 = dict_var['rho_add'] - dict_var['rho_s'] * np.exp( dict_cte['gamma']/2.0 * (dict_var['z_s'] - dict_var['z_add']) / h )
+
+    #Eq 24: At z=z_s the optical depth is 2/3
+
+    # constant in integration
+    b_s = 0.5 * dict_cte['mu_gas'] / 8.314  * dict_cte['omagak']**2 * dict_var['z_s']**2 / dict_var['T_s'] 
+
+    # The int.quad do the integration of the lambda function from 1 to infinity
+    res_24 = 2/3 -  kappa_p * dict_var['rho_s'] * dict_var['z_s'] * int.quad(lambda eps : np.exp(b_s*(1-eps**2)), 1, np.inf)
+
+    #Eq 31 (or 32) The residue of the midplane temperature equation
+
+    #surface mass coordinates
+    q_s = surface_mass_coordinate(kappa_p,dict_var['sigma'])
+
+    res_31 = midplane_temp_residue(dict_var['T_mid'],dict_var['T_s'],dict_cte['gamma'],dict_cte['Chi'],kappa_p0,beta,dict_cte['mu_gas'],dict_cte['alpha'],q_s,dict_cte['cap_lambda'],dict_cte['omegak'],dict_cte['L'])
+
+    #Eq 36: Addiabatique density as function of mid plane density and temperature and photosphere
+    # temperature (assumed to be equal to addiabatique frontier temperature)
+
+    res_36 = dict_var['rho_add'] - dict_var['rho_mid']* (dict_var['T_s'] / dict_var['T_mid'])**(1/(dict_cte['gamma']-1))
+
+    #Eq 37: Addiabatique to isotherm frontier height 
+
+    res_37 = dict_var['z_add'] - np.sqrt( (2 * dict_cte['gamma'] * 8.314) / ( (dict_cte['gamma'] -1 ) * dict_cte['omegak'] ) ) * np.sqrt( dict_var['T_mid'] - dict_var['T_s'] ) / dict_cte['omegak']
+
+    #Eq 39 we should be able to retreive the surface density from the density profile rho(r,z)
+
+    # integration parameters
+    b_a =  0.5 * dict_cte['mu_gas'] / 8.314  * dict_cte['omagak']**2 * dict_var['z_add']**2 / dict_var['T_s'] 
+    zeta_a = dict_var['z_add']/dict_var['z_s']
+
+    # function to integration to get 
+    function_a = lambda zeta : (1 + (dict_cte['gamma'] -1)/dict_cte['gamma'] * b_a * (1 - zeta**2) ) ** (1/(dict_cte['gamma']-1))
+
+    res_39 = 2 * dict_var['z_add'] * dict_var['rho_add'] * int.quad(function_a,0,1) + 2 *dict_var['z_s'] * dict_var['rho_s'] * int.quad(lambda zeta : np.exp( b_s * (1- zeta**2), zeta_a, 1))
+
+    # return all residues as single vector of size 8*Nr
+
+    res_vec = np.concatenate( (res_10,res_17,res_23,res_24,res_31,res_36,res_37,res_39) )
+
+    return res_vec
+
 
 
 #######################################################################################
@@ -264,7 +326,7 @@ dict_cte["Mdot"] = Mdot
 
 
 
-#-----------------Construction of the constant value with temperature dictionary---------#
+#-----------------Dictionary of values constant withh respect to temperature construction---------#
 
 dict_cte['L'] = 1 - np.sqrt(R_p/R_disk)   # momentum transfert coeficient 
 
@@ -272,7 +334,6 @@ dict_cte['L'] = 1 - np.sqrt(R_p/R_disk)   # momentum transfert coeficient
 J = specific_angular_momentum(M_p)  # Specific angular momentum [m².s^-1]
 R_c = J**2 / (cte.G.value * M_p) # Centrifuge radius [m] 
 
-# dict_cte['r'] = np.logspace(np.log10(1.2*R_p),np.log10(100*cte.R_jup.value),Nr) # computational log grid, non linear
 dict_cte['r'] = np.logspace(np.log10(1.2*R_p),np.log10(50*cte.R_jup.value),Nr) # computational log grid, non linear
 
 dict_cte['cap_lambda'] = cap_lambda(dict_cte['r'],R_c,R_p,R_disk) # Lambda from equations 2 and 3 from makalkin 2014
@@ -297,20 +358,9 @@ and viscous heating is a close enougth guess.
 # From eq 4 in Anderson 2021 guess of surface and mid plane temps 
 mid_temp0 = ((3*dict_cte['omegak']**2 * dict_cte['Mdot']*year * dict_cte['cap_lambda']) / (10 * np.pi * cte.sigma_sb.value)  + temp_neb**4)**0.25 
 
-# guess for surface density 
-cs = sound_speed(dict_cte['gamma'],mid_temp0/10,dict_cte["mu_gas"])
-mu = viscosity(cs,dict_cte['alpha'],dict_cte["omegak"])
-sigmag0 = surface_density(dict_cte['Mdot'],mu,dict_cte['cap_lambda'],dict_cte['L'])
-
-# guess opacity 
-kappa0,beta,kappa_p0 = mean_planck_opacity(mid_temp0,dict_cte['Chi'])
-
-#Guess ofr photosphere surface altitude 
-h = gas_scale_height(cs,mid_temp0,dict_cte['omegak'])
-zs0 = z_surface(sigmag0,kappa_p0,h)
 
 
-X0 = np.concatenate((mid_temp0/5,mid_temp0,sigmag0,zs0))
+
 sol = opt.root(Residue,X0,args=(dict_cte,Nr),method='hybr')
 
 print(sol)
